6a71e51c6edbe7eab5d6ae32691db88e3419c191
einnivd <yelk0427@gmail.com>
2026-05-20 15:46:28 +0900

    공지사항 탭 기능, 모달창 열기

---
+++ jquery/js/tab_modal.js
@@ -0,0 +1,17 @@
+$(function () {
+  // 탭 메뉴
+  $(".tabMenu>li>a").click(function () {
+    console.log(this);
+    $(this).parent().addClass("active").siblings().removeClass("active");
+  });
+  // modal open
+  $(".notice li:first-child").click(function () {
+    // $(".modal").addClass("active");
+    $(".modal").show()
+  });
+  // modal close
+  $(".modal-btn").click(function(){
+    // $(".modal").removeClass("active");
+    $(".modal").hide()
+  })
+});
