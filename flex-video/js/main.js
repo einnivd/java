6c0de1cac28a150c54b6cad5e7a4d78674287342
einnivd <yelk0427@gmail.com>
2026-06-01 17:12:46 +0900

    [flex영상] 반응형- max-width: 1000px/768px/390px

---
+++ flex-video/js/main.js
@@ -0,0 +1,48 @@
+window.addEventListener("load", () => {
+  const items = document.querySelectorAll("article");
+  const aside = document.querySelector("aside");
+  // asid 안에 있는 span을 잡을 때
+  const close = aside.querySelector("span");
+  items.forEach((item) => {
+    // console.log(item);
+    // article 호버했을 때
+    item.addEventListener("mouseenter", (e) => {
+      // console.log(e.currentTarget);
+      e.currentTarget.querySelector("video").play();
+    });
+    item.addEventListener("mouseleave", (e) => {
+      e.currentTarget.querySelector("video").pause();
+    });
+    item.addEventListener("click", (e) => {
+      let title = e.currentTarget.querySelector("h2").innerText;
+      console.log(title);
+      let txt = e.currentTarget.querySelector("p").innerText;
+      console.log(txt);
+      let vidSrc = e.currentTarget.querySelector("video").getAttribute("src");
+      console.log(vidSrc);
+      aside.querySelector("h1").innerText = title;
+      aside.querySelector("p").innerText = txt;
+      aside.querySelector("video").setAttribute("src", vidSrc);
+      //   aside 보이게
+      aside.classList.add("on");
+      // aside 요소의 동영상 재생
+      aside.querySelector("video").play();
+      // 스크롤 제거
+      document.body.style.overflow = "hidden";
+      // 스크롤 맨 위로 이동
+      window.scrollTo({
+        top: 0,
+      });
+    });
+
+    // close 클릭시
+    close.addEventListener("click", () => {
+      // aside 안 보이게
+      aside.classList.remove("on");
+      // aside 요소의 동영상 재생
+      aside.querySelector("video").pause();
+      // 스크롤 다시 생성
+      document.body.style.overflow = "auto";
+    });
+  });
+});
